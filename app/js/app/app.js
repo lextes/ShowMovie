98c6cc2569930c1b2fe925b527415f0ed5264cd9
lextes <forza1708@gmail.com>
2016-03-24 12:14:29 -0400

    fixes #4

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -1,7 +1,15 @@
-var genre ["terror", "Comedia", "Infantil", "Drama"];
 
-function genre (){
-	
-	alert()
+function showGenre(genre){
 
+	var genre=genre-1;
+	var genders["Terror", "Comedia", "Infantil", "Drama"]
+
+	if (genders[genre]!=null){
+	return genders[genre]
+} else {
+	throw "NO hay ese genero"
 }
+
+
+
+
