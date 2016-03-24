244369c49cdaa1b8306a5e3b0d309cce739ba53d
lextes <forza1708@gmail.com>
2016-03-23 20:04:32 -0400

    feature/4

---
+++ app/js/app/app.js
@@ -0,0 +1,9 @@
+
+var genre=["Terror", "Comedia", "Infantil", "Drama"];
+
+genre = new Array (4);
+genre[0] = "Terror"
+genre[0] = "Comedia"
+genre[0] = "Infantil"
+genre[0] = "Drama"
+console.log(genre);
