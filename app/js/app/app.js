6d82a325fd06679315a126a82849c73c17a470bd
lextes <forza1708@gmail.com>
2016-03-23 23:57:07 -0400

    fixes #4

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -1,9 +1,12 @@
 
-var genre=["Terror", "Comedia", "Infantil", "Drama"];
-
 genre = new Array (4);
 genre[0] = "Terror"
-genre[0] = "Comedia"
-genre[0] = "Infantil"
-genre[0] = "Drama"
-console.log(genre);
+genre[1] = "Comedia"
+genre[2] = "Infantil"
+genre[3] = "Drama"
+
+function genre (){
+	alert("muevete")
+
+}
+
