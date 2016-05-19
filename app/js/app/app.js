42aef1bb043045d696e25b180ff4d20528e91d87
lextes <forza1708@gmail.com>
2016-05-19 18:33:23 -0400

    fixes #17

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -62,7 +62,7 @@ var movies = [
                title:"Deadpool",
                year:2016,
                summary:"A former Special Forces operative turned mercenary is subjected to a rogue experiment that leaves him with accelerated healing powers, adopting the alter ego Deadpool",
-               genre:"Comedy"
+               genre:"Comedy",
            }
            {
                url:"http://www.imdb.com/title/tt2948356/",
@@ -70,7 +70,7 @@ var movies = [
                title:"Zootopia",
                year:2016,
                summary:"In a city of anthropomorphic animals, a rookie bunny cop and a cynical con artist fox must work together to uncover a conspiracy.",
-               genre:"Comedy"
+               genre:"Comedy",
            }
            {
                url:"http://www.imdb.com/title/tt0993846/",
@@ -78,7 +78,7 @@ var movies = [
                title:"The Wolf of Wall Street",
                year:2013,
                summary:"Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.",
-               genre:"Comedy"
+               genre:"Comedy",
            }
            {
                url:"http://www.imdb.com/title/tt2096673/",
@@ -86,7 +86,7 @@ var movies = [
                title:"Inside Out",
                year:2015,
                summary:"After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.",
-               genre:"Comedy"
+               genre:"Comedy",
            }
            {
                url:"http://www.imdb.com/title/tt0093779/",
@@ -94,7 +94,7 @@ var movies = [
                title:"The Princess BrideThe",
                year:1987,
                summary:"While home sick in bed, a young boy's grandfather reads him a story called The Princess Bride.",
-               genre:"Comedy"
+               genre:"Comedy",
            }
            {
                url:"http://www.imdb.com/title/tt0078748/",
@@ -102,7 +102,7 @@ var movies = [
                title:"Alien",
                year:1979,
                summary:"After a space merchant vessel perceives an unknown transmission as distress call, their landing on the source planet finds one of the crew attacked by a mysterious lifeform. Continuing their journey back to Earth with the attacked crew having recovered and the critter deceased, they soon realize that its life cycle has merely begun.",
-               genre:"horror"
+               genre:"horror",
            }
            {
                url:"http://www.imdb.com/title/tt0081505/",
@@ -110,7 +110,7 @@ var movies = [
                title:"The shining",
                year:1980,
                summary:"A family heads to an isolated hotel for the winter where an evil and spiritual presence influences the father into violence, while his psychic son sees horrific forebodings from the past and of the future.",
-               genre:"horror"
+               genre:"horror",
            }
            {
                url:"http://www.imdb.com/title/tt0054215/",
@@ -118,7 +118,7 @@ var movies = [
                title:"Psycho",
                year:1960,
                summary:"A Phoenix secretary steals $40,000 from her employer's client, goes on the run and checks into a remote motel run by a young man under the domination of his mother.",
-               genre:"horror"
+               genre:"horror",
            }
            {
                url:"http://www.imdb.com/title/tt0090605/",
@@ -126,7 +126,7 @@ var movies = [
                title:"Psycho",
                year:1986,
                summary:"The planet from Alien (1979) has been colonized, but contact is lost. This time, the rescue team has impressive firepower, but will it be enough?",
-               genre:"horror"
+               genre:"horror",
            }
             {
                url:"http://www.imdb.com/title/tt0073195/",
@@ -134,7 +134,7 @@ var movies = [
                title:"Jaws",
                year:1975,
                summary:"When a gigantic great white shark begins to menace the small island community of Amity, a police chief, a marine scientist and a grizzled fisherman set out to stop it.",
-               genre:"horror"
+               genre:"horror",
            }
            {
                url:"http://www.imdb.com/title/tt1663202/",
@@ -142,7 +142,7 @@ var movies = [
                title:"The Revenant",
                year:2015,
                summary:"A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team.",
-               genre:"Drama"
+               genre:"Drama",
            }
            {
                url:"http://www.imdb.com/title/tt3170832/",
@@ -150,7 +150,7 @@ var movies = [
                title:"La habitacion",
                year:2015,
                summary:"A young boy is raised within the confines of a small shed.",
-               genre:"Drama"
+               genre:"Drama",
            }
            {
                url:"http://www.imdb.com/title/tt3659388/",
@@ -158,7 +158,7 @@ var movies = [
                title:"The martian",
                year:2015,
                summary:"An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.",
-               genre:"Drama"
+               genre:"Drama",
            }
            {
                url:"http://www.imdb.com/title/tt0068646/",
@@ -166,7 +166,7 @@ var movies = [
                title:"The Godfather",
                year:1972,
                summary:"The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-               genre:"Drama"
+               genre:"Drama",
            }
            {
                url:"http://www.imdb.com/title/tt1895587/",
@@ -174,7 +174,7 @@ var movies = [
                title:"Spotlight",
                year:2015,
                summary:"The true story of how the Boston Globe uncovered the massive scandal of child molestation and cover-up within the local Catholic Archdiocese, shaking the entire Catholic Church to its core.",
-               genre:"Drama"
+               genre:"Drama",
            }
            {
                url:"http://www.imdb.com/title/tt2488496/",
@@ -182,7 +182,7 @@ var movies = [
                title:"Star Wars: Episode VII - The Force Awakens",
                year: 2015,
                summary:"Three decades after the defeat of the Galactic Empire, a new threat arises. The First Order attempts to rule the galaxy and only a ragtag group of heroes can stop them, along with the help of the Resistance.",
-               genre:"fantasy"
+               genre:"fantasy",
            }
             {
                url:"http://www.imdb.com/title/tt2096673/",
@@ -190,7 +190,7 @@ var movies = [
                title:"Inside Out",
                year: 2015,
                summary:"After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.",
-               genre:"fantasy"
+               genre:"fantasy",
            }
            {
                url:"http://www.imdb.com/title/tt0093779/",
@@ -198,7 +198,7 @@ var movies = [
                title:"The Princess Bride",
                year: 1987,
                summary:"While home sick in bed, a young boy's grandfather reads him a story called The Princess Bride.",
-               genre:"fantasy"
+               genre:"fantasy",
            }
             {
                url:"http://www.imdb.com/title/tt0076759/",
@@ -206,7 +206,7 @@ var movies = [
                title:"Star Wars: Episode IV - A New Hope",
                year: 1977,
                summary:"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
-               genre:"fantasy"
+               genre:"fantasy",
            }
        ];
 
